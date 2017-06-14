7b1d50f8f27f6f73c0ff05ce7acd8082290ed7a9
Mario Alberto Villatoro <ing.mario.villatoro@gmail.com>
2017-06-14 12:01:55 -0500

    Se agregaron los migrations del modulo de pacientes

---
+++ database/migrations/2017_06_13_151043_creacion_tabla_responsable.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreacionTablaResponsable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pacientes_responsable', function (Blueprint $table) {
+            $table->string('id', 255);
+            $table->integer('incremento');
+            $table->string('servidor_id', 4);
+
+            $table->string('paciente_id');
+            $table->string('nombre');
+            $table->string('parentesco');
+            $table->string('domicilio');
+
+            $table->string('telefono');
+            $table->string('usuario_id');
+            
+            $table->primary('id');
+
+            $table->foreign('paciente_id')->references('id')->on('pacientes');
+            
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pacientes_responsable');
+    }
+}
---
+++ database/migrations/2017_06_14_123611_creacion_tabla_area_responsable.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreacionTablaAreaResponsable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pacientes_area_responsable', function (Blueprint $table) {
+            $table->string('id', 255);
+            $table->integer('incremento');
+            $table->string('servidor_id', 4);
+
+            $table->integer('area_responsable_id')->unsigned();
+            $table->string('paciente_id');
+            $table->string('usuario_id');
+            
+            $table->primary('id');
+
+            $table->foreign('area_responsable_id')->references('id')->on('catalogo_area_responsable');
+            $table->foreign('paciente_id')->references('id')->on('pacientes');
+            
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pacientes_area_responsable');
+    }
+}
