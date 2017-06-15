804bf6689dbf02e92dff8045260ebada05a500ad
Mario Alberto Villatoro <ing.mario.villatoro@gmail.com>
2017-06-15 13:00:51 -0500

    actualizacion prueba paciente controlador

--- app/Http/Controllers/AdmisionUnidad/PacienteController.php
+++ app/Http/Controllers/AdmisionUnidad/PacienteController.php
@@ -44,8 +44,8 @@ class PacienteController extends Controller
             $paciente = $paciente->get();
         }*/
 
-        $paciente =  Paciente::all();
-        //$paciente = $paciente->get();
+        $paciente =  Paciente::select('*');
+        $paciente = $paciente->get();
         
         /*$paciente =  Paciente::select('*')->with("localidad.municipio", "Ingresoactivos.Unidad", "responsable");
         $resultadosPorPagina = isset($parametros["per_page"])? $parametros["per_page"] : 20;
