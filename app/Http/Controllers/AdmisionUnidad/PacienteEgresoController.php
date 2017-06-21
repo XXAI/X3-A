453428ef78c462f1a4fd43ce78ba7dc1d0daa982
Mario Alberto Villatoro <ing.mario.villatoro@gmail.com>
2017-06-21 11:43:41 -0500

    Revision modulo paciente egreso

--- app/Http/Controllers/AdmisionUnidad/PacienteEgresoController.php
+++ app/Http/Controllers/AdmisionUnidad/PacienteEgresoController.php
@@ -27,9 +27,9 @@ class PacienteEgresoController extends Controller
     public function index()
     {
 
-    	/*$obj =  JWTAuth::parseToken()->getPayload();
+    	$obj =  JWTAuth::parseToken()->getPayload();
         $usuario = Usuario::where("id", $obj->get('id'))->with("usuariounidad")->first();
-        $clues = $usuario->usuariounidad->clues;    
+        /*$clues = $usuario->usuariounidad->clues;    
 
         $usuarios = UsuarioUnidad::where("clues", $clues)->select("usuario_id")->get();
 
@@ -67,7 +67,7 @@ class PacienteEgresoController extends Controller
             $pacientes_ingreso = $pacientes_ingreso->get();
         }*/
 
-        return Response::json([ 'data' => "hola"],200);
+        return Response::json([ 'data' => $usuario ],200);
     }
 
     public function store(Request $request)
