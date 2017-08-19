4acfd06083bb05740efa8b3cc719bdc3c7c01655
Mario Alberto Villatoro <ing.mario.villatoro@gmail.com>
2017-08-19 09:53:44 -0500

    Actualizacion aplicacion de avance

--- app/Http/Controllers/AvanceController.php
+++ app/Http/Controllers/AvanceController.php
@@ -9,6 +9,7 @@ use JWTAuth;
 use App\Http\Requests;
 use App\Models\Avance;
 use App\Models\AvanceDetalles;
+use App\Models\Usuario;
 use Carbon\Carbon;
 
 use Illuminate\Support\Facades\Input;
@@ -16,11 +17,60 @@ use \Validator,\Hash, \Response, DB;
 
 class AvanceController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         
         $parametros = Input::only('status','q','page','per_page');
-		$avance = Avance::getModel();
+		
+        $usuario = Usuario::find($request->get('usuario_id'));
+
+        $usuario_general = Usuario::with(['roles.permisos'=>function($permisos){
+                $permisos->where('id','79B3qKuUbuEiR2qKS0CFgHy2zRWfmO4r');
+            }])->find($request->get('usuario_id'));
+        $general    = false;
+        $normal     = false;
+        $permisos = [];
+        if($usuario_general->su == 1)
+            $general = true;
+        else{
+            foreach ($usuario_general->roles as $index => $rol) {
+                foreach ($rol->permisos as $permiso) {
+                    $permisos[$permiso->id] = true;
+
+                    if(count($permisos)){
+                        $general = true;
+                    }
+                }
+            }
+            if(count($permisos)){
+                $general = true;
+            }
+            
+            
+            $usuario_normal = Usuario::with(['roles.permisos'=>function($permisos){
+                    $permisos->where('id','f0CT1EvFcj4hqK1rNEpsSXhAlhdE9duM');
+                }])->find($request->get('usuario_id'));
+                $permisos_normal = [];
+                foreach ($usuario->roles as $index => $rol) {
+                    foreach ($rol->permisos as $permiso) {
+                        $permisos_normal[$permiso->id] = true;
+                    }
+                }
+                if(count($permisos_normal)){
+                    $normal = true;
+                }
+        }    
+
+        //return Response::json([ 'data' => $general." , ".$normal],200);
+		if($general)
+			$avance = DB::table('avances')->whereNull("deleted_at");
+		else if($normal)
+		{
+			$avance = DB::table('avances')->where("usuario_id", $request->get('usuario_id'))->whereNull("deleted_at");
+		}else
+		{
+			return Response::json(['error' => "Error, no tiene permisos "], 500); 
+		}
 		
        if ($parametros['q']) {
             $avance =  $avance->where(function($query) use ($parametros) {
@@ -34,21 +84,36 @@ class AvanceController extends Controller
         } else {
             $avance = $avance->get();
         }
-
+         
         foreach ($avance as $key => $value) {
-        	$avanceDetalle = AvanceDetalles::where("avance_id", $avance[$key]['id'])->orderBy('id', 'desc')->first();
+        	$avanceDetalle = AvanceDetalles::where("avance_id", $avance[$key]->id)->orderBy('id', 'desc')->first();
         	if($avanceDetalle)
         	{
-        		$avance[$key]['porcentaje'] = $avanceDetalle->porcentaje;
-        		$avance[$key]['creacion'] = $avanceDetalle->created_at;
+        		$avance[$key]->porcentaje = $avanceDetalle->porcentaje;
+                $array_days = explode("-", substr($avanceDetalle->created_at,0,10));
+                $avance[$key]->creacion = Carbon::createFromDate($array_days[0],$array_days[1],$array_days[2])->diffInDays(Carbon::now());
+                $avance[$key]->comenntario_detalle = $avanceDetalle->comentario;
         	}
         	else
         	{
-        		$avance[$key]['porcentaje'] = "0";
-        		$avance[$key]['creacion'] = Null;
+        		$avance[$key]->porcentaje = "0.00";
+        		$avance[$key]->creacion = -1;
+                $avance[$key]->comenntario_detalle = '';
         	}
         }
-       
+        $aux = array();
+        foreach ($avance as $key => $value) {
+            foreach ($avance as $key2 => $value2) {
+                if($avance[$key]->creacion < $avance[$key2]->creacion)
+                 {
+                     $aux = $avance[$key];
+                     $avance[$key] = $avance[$key2];
+                     $avance[$key2] = $aux;   
+                 }   
+            }
+            
+        }
+
         return Response::json([ 'data' => $avance],200);
     }
 
