cd8673992f188198e2a9f9f031314739d43f01b2
Mario Alberto Villatoro <ing.mario.villatoro@gmail.com>
2017-09-04 15:19:42 -0500

    Actualizacion api se agregan permisos del modulo de avance

---
+++ database/seeds/PermisoAvanceSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PermisoAvanceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('permisos')->insert([
+            [
+                'id' => 'WbBYhMFZkGsAYeN13hY1hylZkNPJbHOE',
+                'descripcion' => 'Modulo de Avances',
+                'grupo' => 'Avances',
+                'su' => 0,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+ 	    [
+                'id' => '79B3qKuUbuEiR2qKS0CFgHy2zRWfmO4r',
+                'descripcion' => 'Usuario General',
+                'grupo' => 'Avances',
+                'su' => 0,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+ 	    [
+                'id' => 'f0CT1EvFcj4hqK1rNEpsSXhAlhdE9duM',
+                'descripcion' => 'Usuario Normal',
+                'grupo' => 'Avances',
+                'su' => 0,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+    }
+}
