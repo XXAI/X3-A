5fccd8f2544ca311d676a3297ca47e200a327fef
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2018-06-15 15:24:17 -0500

    Selibera parche 3 de la api

--- config/patches.php
+++ config/patches.php
@@ -9,6 +9,7 @@
 
 
 return [	
+	[ "nombre" => "35915E24B6C820FB9EB4847E05A4AC6F.sial.api.3.patch", "fecha" => "2018-06-15", "ejecutar" =>  "\App\Librerias\Patches\Parches::ejecutar"],
 	[ "nombre" => "B8DD2113084EC4D3B2E97F081BD081E4.sial.api.2.patch", "fecha" => "2018-03-16", "ejecutar" =>  "\App\Librerias\Patches\Parches::ejecutar"],
 	[ "nombre" => "F24DBF6BE70AB8B018323D6EA4601455.sial.api.1.patch", "fecha" => "2018-03-16", "ejecutar" =>  "\App\Librerias\Patches\Parches::ejecutar"],
 ];
