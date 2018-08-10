1415f4673c3ca8b6653aa670a2d23092db804203
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2018-08-10 12:24:13 -0500

    parche 4 - 25C6083F763EB4F414EC55FC3283F7B7.sial.api.4.patch

--- config/patches.php
+++ config/patches.php
@@ -9,7 +9,7 @@
 
 
 return [
-	[ "nombre" => "25C6083F763EB4F414EC55FC3283F7B7.sial.api.4.patch", "fecha" => "2018-06-15", "ejecutar" =>  "\App\Librerias\Patches\Parches::ejecutarParche4"],
+	[ "nombre" => "25C6083F763EB4F414EC55FC3283F7B7.sial.api.4.patch", "fecha" => "2018-08-10", "ejecutar" =>  "\App\Librerias\Patches\Parches::ejecutarParche4"],
 	[ "nombre" => "35915E24B6C820FB9EB4847E05A4AC6F.sial.api.3.patch", "fecha" => "2018-06-15", "ejecutar" =>  "\App\Librerias\Patches\Parches::ejecutar"],
 	[ "nombre" => "B8DD2113084EC4D3B2E97F081BD081E4.sial.api.2.patch", "fecha" => "2018-03-16", "ejecutar" =>  "\App\Librerias\Patches\Parches::ejecutar"],
 	[ "nombre" => "F24DBF6BE70AB8B018323D6EA4601455.sial.api.1.patch", "fecha" => "2018-03-16", "ejecutar" =>  "\App\Librerias\Patches\Parches::ejecutar"],
