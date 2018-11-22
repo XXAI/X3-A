b7c70fe4502aef0c9011dba21b15bae1b2eefa4d
Akira Redwolf <akira.redwolf@gmail.com>
2018-11-21 21:59:09 -0600

    Este commit es solo por si toman la secretaría

---
+++ database/migrations/2018_11_21_183010_create_pedidos_ordinarios.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePedidosOrdinarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos_ordinarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('descripcion', 255);
+			$table->date('fecha');
+            $table->date('fecha_expiracion');
+            $table->string('usuario_id', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pedidos_ordinarios');
+    }
+}
---
+++ database/migrations/2018_11_21_183902_create_pedidos_ordinarios_unidades_medicas.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePedidosOrdinariosUnidadesMedicas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos_ordinarios_unidades_medicas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pedido_ordinario_id')->unsigned();
+            $table->string('pedido_id', 255)->nullable();
+            $table->string('clues', 45);
+            $table->decimal('causes_autorizado',15,2)->default(0);
+            $table->decimal('causes_modificado',15,2)->default(0);
+            $table->decimal('no_causes_autorizado',15,2)->default(0);
+            $table->decimal('no_causes_modificado',15,2)->default(0);
+            $table->timestamps();
+
+            $table->foreign('pedido_ordinario_id')->references('id')->on('pedidos_ordinarios');
+            $table->foreign('pedido_id')->references('id')->on('pedidos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pedidos_ordinarios_unidades_medicas');
+    }
+}
